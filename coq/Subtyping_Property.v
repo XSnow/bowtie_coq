e0272c8a43ed28b0f76542f0e9a91f8ee7482a29
xsnow <xsnow@outlook.com>
2021-03-03 04:43:34 +0800

    prove distAnd is not necessary in declrative sub

--- coq/Subtyping_Property.v
+++ coq/Subtyping_Property.v
@@ -561,3 +561,32 @@ Proof.
   - applys~ singlemode_sub_orl H1.
   - applys~ singlemode_sub_orr H1.
 Qed.
+
+(************************************************************)
+(* Prove distand is not necessary *)
+Hint Constructors declarative_subtyping_distor : core.
+Lemma distor_symm_or : forall A1 A2,
+    declarative_subtyping_distor (t_or A1 A2) (t_or A2 A1).
+Proof.
+  introv.
+  applys~ DSO_or.
+Qed.
+
+Theorem distor_eqv_distand : forall A1 A2 B,
+    declarative_subtyping_distor (t_and (t_or A1 A2) B) (t_or (t_and A1 B) (t_and A2 B)).
+Proof.
+  introv.
+  applys DSO_trans.
+  2: { applys DSO_distOr. }
+  applys DSO_and.
+  1: { applys DSO_trans (t_or (t_and A2 B) A1).
+       applys DSO_trans.
+       2: { applys DSO_distOr. }
+       applys~ DSO_and.
+       1: { applys DSO_trans. applys~ DSO_andl. applys~ DSO_or.
+       }
+       2: { applys~ DSO_or. }
+       applys DSO_trans. applys~ DSO_andr. applys~ DSO_orl.
+  }
+  applys DSO_trans. applys~ DSO_andr. applys~ DSO_orl.
+Qed.
