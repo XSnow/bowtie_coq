79fb7edfe0f66b82278bd276562238550d0304d6
xsnow <xsnow@outlook.com>
2021-10-09 04:22:13 +0800

    change tactic skip

--- coq/LibTactics.v
+++ coq/LibTactics.v
@@ -4767,7 +4767,7 @@ Ltac skip_with_existential :=
   match goal with |- ?G =>
     let H := fresh "TEMP" in evar(H:G); eexact H end.
 
-Axiom skip_axiom : False.
+Axiom skip_axiom : True.
   (* To obtain a safe development, change to [skip_axiom : True] *)
 Ltac skip_with_axiom :=
   elimtype False; apply skip_axiom.
